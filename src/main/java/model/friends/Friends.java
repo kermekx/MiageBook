261c8e924ab08468391cba04c8a3feb417dacf6e
Messiaen Kevin <kevin.messiaen@etudiant.univ-lille1.fr>
2018-04-17 14:37:04 +0200

    friends a

--- src/main/java/model/friends/Friends.java
+++ src/main/java/model/friends/Friends.java
@@ -1,5 +1,23 @@
 package model.friends;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Friends {
+
+	public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS FRIENDS ("
+			+ "USERNAME_LEFT CHAR(32) NOT NULL," + "USERNAME_RIGHT CHAR(32) NOT NULL)";
+
+	public String username;
+	public List<String> friends = new ArrayList<>();
+
+	public void addFriend(String username) {
+		if (!isFriend(username))
+			friends.add(username);
+	}
+	
+	public boolean isFriend(String username) {
+		return friends.contains(username);
+	}
 
 }
